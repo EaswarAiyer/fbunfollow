7e3eead511b5b48fe3fe06c4c9448a50348c534f
easwar aiyer <easwar.vaidhyanathan>
2020-08-08 19:00:28 +0530

    added files

---
+++ gotoProfile.js
@@ -0,0 +1,10 @@
+console.log("calling gotoProfile");
+document.getElementById('u_ps_0_1_3').click();
+let editPreferences = document.getElementsByClassName('_54nc');
+console.log(editPreferences);
+editPreferences[2].click();
+let UnfollowOption = document.getElementsByClassName('_2-km');
+console.log(UnfollowOption);
+console.log(UnfollowOption[1]);
+
+
