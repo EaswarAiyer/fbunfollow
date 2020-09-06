ced6d6e4607168769a2e5f8e7c35d820f83f5db7
Antonyrahul <antonyrahul96@gmail.com>
2020-09-06 11:00:17 +0530

    UI revamp, Icon added

--- popup.js
+++ popup.js
@@ -7,23 +7,54 @@ var progressvalue =0;
 var progressbar
 var ptext= "Initializing...";
 let unfollowButton = document.getElementById('unfollowButton');
+let maindiv = document.getElementById("maindiv")
 //let unfollowProgress = document.getElementsByClassName('progressbar');
 unfollowButton.onclick = function(element) {
-  progressbar = document.createElement('progress')
-  progressbar.id="progressbar";
-  progressbar.max = 100
-  progressbar.value = progressvalue;
+  // progressbar = document.createElement('progress')
+  // progressbar.id="progressbar";
+  // progressbar.max = 100
+  // progressbar.value = progressvalue;
+unfollowButton.disabled ="true"
+unfollowButton.style.cursor="not-allowed"
+  let bootprogmain = document.createElement('div')
+  bootprogmain.classList.add("progress")
+  bootprogmain.style="height:15px;margin-top:5px"
+  bootprogmain.setAttribute("position","absolute")
+
+  let bootprogsec = document.createElement('div')
+  bootprogsec.classList.add("progress-bar","progress-bar-striped","active")
+  bootprogsec.role ="progressbar";
+  bootprogsec.setAttribute("role","progressbar")
+  bootprogsec.setAttribute("aria-valuemin",0)
+  bootprogsec.setAttribute("aria-valuemax",100)
+ 
+  
+  //bootprogsec.setAttribute("aria-valuemin","0")
+  bootprogsec.setAttribute("aria-valuenow","50")
+  bootprogsec.style="width:"+progressvalue+"%;"
+  console.log(bootprogsec.style)
+
+  let progresstext = document.createElement('p')
+  progresstext.style="padding-top:0px"
+  progresstext.innerHTML = "Initializing..."
+  bootprogmain.appendChild(bootprogsec);
+  //bootprogmain.appendChild(progresstext)
+
+
 
   chrome.storage.sync.set({"progbartext":"Initializing..."}, function() {
     console.log('Value is set to initialize');
   });
+ 
+  //document.body.appendChild(bootprogmain)
   //progressbar.innerHTML = '<progress id="file" value="32" max="100"> 32% </progress>'
-  document.body.appendChild(progressbar)
+  //document.body.appendChild(progressbar)
 
-  let progresstext = document.createElement('p')
-  progresstext.innerHTML = "Initializing..."
-  document.body.appendChild(progresstext)
 
+ 
+  //document.body.appendChild(progresstext)
+  maindiv.appendChild(bootprogmain)
+  maindiv.appendChild(progresstext)
 
     let color = element.target.value;
   //  unfollowProgress[0].style.visibility = 'visible';
@@ -48,8 +79,12 @@ unfollowButton.onclick = function(element) {
       console.log(progressvalue)
       console.log(progresstext)
      //progressvalue= progressvalue+10
-      progressbar.value = progressvalue;
-      progresstext.innerHTML= ptext
+     // progressbar.value = progressvalue;
+      bootprogsec.style="width:"+progressvalue+"%;"
+     
+      progresstext.innerHTML=ptext;
+     
+     
     }, 1000);
     
   };
